9592f0a2cec35bff14908b806d357c4edaf37f8b
Oğuz Kurt <kurt.oguz@outlook.com>
2024-03-08 12:08:58 +0300

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024tuis",
+      "defaultValue": "2024tuis2",
       "required": "true"
     },
     { "name": "Match Level",
